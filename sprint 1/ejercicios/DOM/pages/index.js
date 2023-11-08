53637a5663e39186a8917d30c078bcfc3118392c
Álvaro Orquera <alvarosop23@gmail.com>
2023-11-08 02:40:08 -0300

    voy haciendo el dom

---
+++ sprint 1/ejercicios/DOM/pages/index.js
@@ -0,0 +1,8 @@
+//ejercicio 1
+const tituloPorId= document.getElementById( `titulo` )
+//ejercicio 2
+const tituloPrincipal = `Frutas`
+//ejercicio 3
+document.getElementById( `titulo` ).innerHTML = tituloPrincipal  
+//ejercicio 4
+
